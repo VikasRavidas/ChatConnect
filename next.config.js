1d51e9d3bf170bd8e540ccb7c897552cab5dc80e
Vikas  Ravidas <126586254+Vikas123eng@users.noreply.github.com>
2025-05-05 10:15:57 +0530

    config: update next.js image optimization settings

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["randomuser.me"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        port: '',
-        pathname: '/api/portraits/**',
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**",
       },
     ],
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
